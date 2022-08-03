438d246014a27d96aa46c26a35bed739fa1e5a44
BartlomiejKita <bartlomiejpawelkita@gmail.com>
2022-08-03 13:24:48 +0200

    add css for profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,28 +1,103 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  margin: auto;
+  max-width: 280px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  border: 1px solid #a2b9bc;
+  border-radius: 5px;
+`;
+
+const Description = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  background: white;
+  padding: 30px;
+  border-bottom: 1px solid #a2b9bc;
+`;
+
+const Avatar = styled.img`
+  width: 100px;
+  border-radius: 50%;
+  background-color: #c2d4dd;
+  margin-bottom: 10px;
+`;
+
+const Name = styled.p`
+  font-weight: bold;
+  margin: 5px 0;
+`;
+
+const Tag = styled.p`
+  margin: 5px 0;
+  color: #a2b9bc;
+`;
+
+const Location = styled.p`
+  margin: 5px 0;
+  color: #a2b9bc;
+  font-weight: bold;
+`;
+
+const Stats = styled.ul`
+  list-style: none;
+  display: flex;
+  background-color: #f0f0f0;
+  margin: 0px;
+  padding: 0px;
+`;
+
+const List = styled.li`
+  display: flex;
+  flex-direction: column;
+  flex-basis: calc(100% / 3);
+  padding: 20px 5px;
+  text-align: center;
+  &:not(:last-child) {
+    border-right: 1px solid #a2b9bc;
+  }
+`;
+
+const Label = styled.span`
+  margin-bottom: 5px;
+  color: #a2b9bc;
+  text-transform: capitalize;
+`;
+
+const Qantity = styled.span`
+  color: black;
+  font-weight: bold;
+`;
+
 const Profile = ({ username, tag, location, avatar, stats }) => {
   return (
-    <div className="profile">
-      <div className="description">
-        <img src={avatar} alt="User avatar" className="avatar" />
-        <p className="name">{username}</p>
-        <p className="tag">{tag}</p>
-        <p className="location">{location}</p>
-      </div>
+    <Wrapper>
+      <Description>
+        <Avatar src={avatar} alt="User avatar" />
+        <Name>{username}</Name>
+        <Tag>@{tag}</Tag>
+        <Location>{location}</Location>
+      </Description>
 
-      <ul className="stats">
-        <li>
-          <span className="label">{Object.keys(stats)[0]}</span>
-          <span className="quantity">{stats.followers}</span>
-        </li>
-        <li>
-          <span className="label">{Object.keys(stats)[1]}</span>
-          <span className="quantity">{stats.views}</span>
-        </li>
-        <li>
-          <span className="label">{Object.keys(stats)[2]}</span>
-          <span className="quantity">{stats.likes}</span>
-        </li>
-      </ul>
-    </div>
+      <Stats>
+        <List>
+          <Label>{Object.keys(stats)[0]}</Label>
+          <Qantity>{stats.followers}</Qantity>
+        </List>
+        <List>
+          <Label>{Object.keys(stats)[1]}</Label>
+          <Qantity>{stats.views}</Qantity>
+        </List>
+        <List>
+          <Label>{Object.keys(stats)[2]}</Label>
+          <Qantity>{stats.likes}</Qantity>
+        </List>
+      </Stats>
+    </Wrapper>
   );
 };
 
